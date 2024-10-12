f6eb67436bd2d06db18bf56931324d96516cb883
ugpu <fashiorg@gmail.com>
2024-10-12 18:00:33 +0800

    init home page

---
+++ app/login/page.tsx
@@ -0,0 +1,15 @@
+export default function LoginPage() {
+    return (
+      <div>
+        <h1>Login</h1>
+        <form>
+          <label>Email:</label>
+          <input type="email" />
+          <label>Password:</label>
+          <input type="password" />
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    );
+  }
+  
---
+++ app/register/page.tsx
@@ -0,0 +1,15 @@
+export default function RegisterPage() {
+    return (
+      <div>
+        <h1>Register</h1>
+        <form>
+          <label>Email:</label>
+          <input type="email" />
+          <label>Password:</label>
+          <input type="password" />
+          <button type="submit">Register</button>
+        </form>
+      </div>
+    );
+  }
+  
